cf1ebd70adee928ea4829a8ae8b17d57c36ac3cd
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-02-13 09:49:15 -0500

    add content to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Jessica (JMF) Arnieri Hernandez Blank Template</title>
+</head>
+
+<body>
+    <h1>Blank Wordpress Template</h1>
+    <p>This is my WordPress template.</p>
+</body>
+
+</html>
