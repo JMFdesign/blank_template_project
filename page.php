d360adc24b4e96282e2dae1ecf78783ebbebe12b
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-02-13 17:36:17 -0500

    add a sidebar to page.php and single.php, add content sidebar.php

--- page.php
+++ page.php
@@ -1,21 +1,25 @@
 <?php get_header(); ?>
 
   <section class="row">
-    <div class="twelve columns">
+    <div class="eight columns">
 
-<!-- BEGIN PAGE PHP -->
+<!-- Begin Loop -->
 			<?php if ( have_posts() ) {
         while( have_posts() ) {
           /* OUR DATA CONTEXT IS DEFINED 	*/
           the_post(); ?>
 
           <h2><?php the_title(); ?></h2>
-					<?php the_content();
+          <?php the_content();
         }
       } ?>
-<!-- END PAGE PHP -->
-
+<!-- End Loop -->
     </div>
+
+    <div class="four columns">
+      <?php get_sidebar(); ?>
+    </div>
+
   </section>
 
 <?php get_footer(); ?>
