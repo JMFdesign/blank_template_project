4b4654e983906189e9ac784d78bd3cebed624984
JMF-Arnieri-Hernandez <jmfa89@ufl.edu>
2017-02-13 17:00:40 -0500

    make index.php dynamic - pull content into the loop

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
           <?php
             if ( have_posts() ) {
               while ( have_posts() ) {
-                the_post();
-              }
-            }
+                the_post(); ?>
+
+                <h3><?php the_title(); ?></h3>
+                <?php the_excerpt(); ?>
+
+              <?php
+              } //end while
+            } //end if
           ?>
           <!-- End Loop -->
         </div>
